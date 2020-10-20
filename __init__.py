4b3fd06685612cc5a54ef4df9fde992ff3471c42
Eric Drechsler <eric.drechsler@cern.ch>
2020-10-20 16:13:46 -0700

    Prepare DiVAE cleanup

---
+++ __init__.py
@@ -0,0 +1,21 @@
+""" Package wide definition of logging style
+
+@author Eric "Dr. Dre" Drechsler (eric.drechsler@cern.ch)
+"""
+import logging
+
+logging.addLevelName( logging.INFO,    "\033[1;95m{0}\033[1;0m".format('INFO '))
+logging.addLevelName( logging.DEBUG,   "\033[1;96m{0}\033[1;0m".format('DEBUG'))
+logging.addLevelName( logging.WARNING, "\033[1;93m{0}\033[1;0m".format('WARN '))
+logging.addLevelName( logging.ERROR,   "\033[1;91m{0}\033[1;0m".format('ERROR'))
+
+#add user defined level
+logging.addLevelName( 9 ,              "\033[1;92m%{0}\033[1;0m".format('NOTIMPLEMENTED'))
+
+BOLD = "\033[1m"
+RESET = "\033[0m"
+logging.basicConfig( level=logging.INFO, format='{0}[%(asctime)s.%(msecs)03d]{1} %(levelname)8s  {0}%(name)-50s{1}%(message)s'.format(BOLD,RESET),datefmt="%H:%M:%S")
+
+def exit():
+    import sys
+    sys.exit()
