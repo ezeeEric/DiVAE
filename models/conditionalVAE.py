190a5e42da47f46ac2d0eb49e64ad2ef2eb77ff1
Eric Drechsler <eric.drechsler@cern.ch>
2020-11-12 16:54:32 -0800

    Add new subdirectory for code resturcturing

---
+++ models/conditionalVAE.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Discrete Variational Autoencoder Class Structures
+
+Author: Eric Drechsler (eric_drechsler@sfu.ca)
+"""
+
+import torch
+from diVAE import VariationalAutoEncoder
+
+
+def ConditionalVariationalAutoEncoder(VariationalAutoEncoder):
+    def __init__(self):
+        pass
+
+
+
