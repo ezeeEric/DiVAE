ae86245ffa603fefc081f2cfe639c231c4fcc53a
Eric Drechsler <eric.drechsler@cern.ch>
2020-11-19 14:03:36 -0800

    Add sequential VAE

---
+++ models/sequentialVAE.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Sequential Variational Autoencoder
+
+A architecture using multiple VAEs, where each VAE is conditioned on the
+previous VAEs input. This is to create a hierarchical dependency if there is two
+or more input images which depend on each other.
+
+Author: Eric Drechsler (eric_drechsler@sfu.ca)
+"""
+
+import torch
+from diVAE import VariationalAutoEncoder
+
+class SequentialVariationalAutoEncoder(VariationalAutoEncoder):
+
+    def __init__(self, **kwargs):
+        super(SequentialVariationalAutoEncoder, self).__init__(**kwargs)
+        self._type="cVAE"
+        #define network structure
+        self._encoder_nodes=[]
+        self._decoder_nodes=[]
+        
+        enc_node_list=[self._input_dimension+1]+self._config.encoder_hidden_nodes
+
+        for num_nodes in range(0,len(enc_node_list)-1):
+            nodepair=(enc_node_list[num_nodes],enc_node_list[num_nodes+1])
+            self._encoder_nodes.append(nodepair)
+        
+        self._reparam_nodes=(self._config.encoder_hidden_nodes[-1],self._latent_dimensions)
+        
+        dec_node_list=[self._latent_dimensions+1]+self._config.decoder_hidden_nodes+[self._input_dimension]
+
+        for num_nodes in range(0,len(dec_node_list)-1):
+            nodepair=(dec_node_list[num_nodes],dec_node_list[num_nodes+1])
+            self._decoder_nodes.append(nodepair)
+        pass
+
+    def forward(self, x, label):
+        x_transformed=x.view(-1, self._input_dimension)
+        label_unsqueezed=label.unsqueeze(-1)
+        x_cat=torch.cat([x_transformed,label_unsqueezed],dim=1)
+        z = self.encoder.encode(x_cat)
+        mu = self._reparam_layers['mu'](z)
+        logvar = self._reparam_layers['var'](z)
+        zeta = self.reparameterize(mu, logvar)
+        zeta_cat=torch.cat([zeta,label_unsqueezed],dim=1)
+        x_recon = self.decoder.decode(zeta_cat)
+        return x_recon, mu, logvar, zeta_cat
+
+    def generate_samples(self,n_samples_per_nr=5, nrs=[0,1,2]):
+        """ 
+        Similar to fwd. only skip encoding part...
+        """
+        outlist=[]
+        for i in nrs:
+            rnd_input=torch.randn((n_samples_per_nr,self._reparam_nodes[1]))
+            target=torch.full((n_samples_per_nr, 1), i, dtype=torch.float32)
+            rnd_input_cat=torch.cat([rnd_input,target], dim=1)
+            output = self.decoder.decode(rnd_input_cat)
+            outlist.append(output)
+        return torch.cat(outlist)
