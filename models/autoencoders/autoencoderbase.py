Fixed energy computation, Added forward calls to dvaepp relevant models

models/autoencoders/autoencoderbase.py
@@ -83,7 +83,7 @@ class AutoEncoderBase(nn.Module):
         parameter_string="\n".join([str(par.shape) if isinstance(par,torch.Tensor) else str(par)  for par in self.__dict__.items()])
         return parameter_string
     
-    def forward(self, input_data):
+    def forward(self, x):
         """[summary]
 
         Args:

models/autoencoders/discreteVAE.py
@@ -338,24 +338,26 @@ class DiVAE(AutoEncoderBase):
 
 		return output
 
-	def forward(self, input_data):
+	def forward(self, x):
 		logger.debug("forward")
 
 		#see definition for explanation
 		out=self._output_container.clear()
-
+        
 		#TODO data prep - study if this does good things
-		input_data_centered=input_data.view(-1, self._flat_input_size)-self._dataset_mean
-		#Step 1: Feed data through encoder 
+		input_data_centered=x.view(-1, self._flat_input_size)-self._dataset_mean
+        
+		#Step 1: Feed data through encoder
 		out.posterior_distributions, out.posterior_samples = self.encoder.hierarchical_posterior(input_data_centered)
+        
 		posterior_samples_concat=torch.cat(out.posterior_samples,1)
 		#Step 2: take samples zeta and reconstruct output with decoder
-		output_activations = self.decoder.decode(posterior_samples_concat)
-
+		output_activations = self.decoder(posterior_samples_concat)
+        
 		out.output_activations = output_activations+self._train_bias
 		out.output_distribution = Bernoulli(logits=out.output_activations)
 		out.output_data = torch.sigmoid(out.output_distribution.logits)
-		
+        
 		return out
 
 if __name__=="__main__":

models/autoencoders/dvaepp.py
@@ -7,6 +7,9 @@ Author: Abhi (abhishek@myumanitoba.ca)
 # PyTorch imports
 import torch
 
+# Torchviz imports
+from torchviz import make_dot
+
 # DiVAE imports
 from models.autoencoders.discreteVAE import DiVAE
 from models.rbm.rbm import RBM
@@ -51,16 +54,63 @@ class DiVAEPP(DiVAE):
         Returns:
             PCD Sampler
         """
-        return PCD(batchSize=32, RBM=self.prior, n_gibbs_sampling_steps=40)
+        return PCD(batchSize=128, RBM=self.prior, n_gibbs_sampling_steps=40)
     
-    def create_networks(self):
-        logger.debug("Creating Network Structures")
-        self.encoder = self._create_encoder()
-        self.prior = self._create_prior()
-        self.decoder = self._create_decoder()
-        self.sampler = self._create_sampler()
+    def forward(self, x):
+        """
+        - Overrides forward in discreteVAE.py
         
-    def kl_divergence(self, post_dists, post_samples, is_training=True):
+        Returns:
+            out: output container 
+        """
+        logger.debug("forward")
+        
+        #see definition for explanation
+        out=self._output_container.clear()
+      	
+	#TODO data prep - study if this does good things
+        input_data_centered=x.view(-1, self._flat_input_size)-self._dataset_mean
+        
+	#Step 1: Feed data through encoder
+        out.beta, out.post_logits, out.post_samples = self.encoder(input_data_centered)
+        post_samples = torch.cat(out.post_samples, 1)
+        
+        output_activations = self.decoder(post_samples)
+        out.output_activations = output_activations+self._train_bias
+        out.output_distribution = Bernoulli(logits=out.output_activations)
+        out.output_data = torch.sigmoid(out.output_distribution.logits)
+        
+        """
+        Autograd graph visualization
+        
+        # Create the dot objects using torchviz
+        post_logits_dot = make_dot(torch.cat(out.post_logits, 1), params=dict(self.encoder.named_parameters()))
+        post_samples_dot = make_dot(post_samples, params=dict(self.encoder.named_parameters()))
+        
+        # Save the dot objects
+        post_logits_dot.save(filename='post_logits.dot')
+        post_samples_dot.save(filename='post_samples.dot')
+        
+        output_activations_dot = make_dot(output_activations)
+        output_activations_dot.save(filename='output_activations.dot')
+        """
+        
+        return out
+    
+    def loss(self, input_data, fwd_out):
+	    logger.debug("loss")
+    
+	    kl_loss=self.kl_divergence(fwd_out.beta, fwd_out.post_logits, fwd_out.post_samples)
+
+	    ae_loss_matrix=-fwd_out.output_distribution.log_prob_per_var(input_data.view(-1, self._flat_input_size))
+	    ae_loss_per_sample = torch.sum(ae_loss_matrix,1)
+	    ae_loss = torch.mean(ae_loss_per_sample)
+
+	    loss = ae_loss + kl_loss
+ 
+	    return {"loss":loss, "ae_loss":ae_loss, "kl_loss":kl_loss}
+        
+    def kl_divergence(self, beta, post_logits, post_samples, is_training=True):
         """
         - Compute KLD b.w. hierarchical posterior and RBM prior for DVAE++
         - See (https://github.com/QuadrantAI/dvae/blob/master/rbm.py#L139) for original 
@@ -81,16 +131,16 @@ class DiVAEPP(DiVAE):
         log_ratio = []
         
         # Compute and sum the entropy for each hierarchy level
-        for factorial, samples in zip(post_dists, post_samples):
+        for logits, samples in zip(post_logits, post_samples):
+            factorial = self.encoder.smoothing_dist(logits=logits, beta=beta)
             entropy += torch.sum(factorial.entropy(), 1)
-            logit_q.append(factorial.logits)
             log_ratio.append(factorial.log_ratio(samples))
             
-        logit_q = torch.cat(logit_q, 1)
+        logit_q = torch.cat(post_logits, 1)
         log_ratio = torch.cat(log_ratio, 1)
         samples = torch.cat(post_samples, 1)
         
-        num_latent_layers = len(post_dists)
+        num_latent_layers = len(post_logits)
         
         # Mean-field solution for num_latent_layers == 1
         if num_latent_layers == 1:
@@ -106,13 +156,13 @@ class DiVAEPP(DiVAE):
         rbm_vis = rbm_visible_samples.detach()
         rbm_hid = rbm_hidden_samples.detach()
         
-        batch_energy = (torch.sum(torch.matmul(rbm_vis, torch.matmul(self.prior.get_weights(), rbm_hid.t())), 1) 
-                        + torch.matmul(rbm_vis, self.prior.get_visible_bias())
-                        + torch.matmul(rbm_hid, self.prior.get_hidden_bias()))
-        neg_energy = - torch.mean(batch_energy)
+        batch_energy = (- torch.sum(torch.matmul(rbm_vis, torch.matmul(self.prior.get_weights(), rbm_hid.t())), 1) 
+                        - torch.matmul(rbm_vis, self.prior.get_visible_bias())
+                        - torch.matmul(rbm_hid, self.prior.get_hidden_bias()))
+        neg_energy = - torch.mean(batch_energy, 0)
         cross_entropy = neg_energy + cross_entropy
 
-        kl_loss = cross_entropy - entropy
+        kl_loss = cross_entropy - torch.mean(entropy, 0)
         
         return kl_loss
     
@@ -140,7 +190,7 @@ class DiVAEPP(DiVAE):
         q1_pert = torch.sigmoid(logit_q1 + log_ratio_1)
         
         cross_entropy = (- torch.matmul(q1, self.prior.get_visible_bias())
-                         - torch.matmul(q2, self.prior.get_hidden_bias()) 
+                         - torch.matmul(q2, self.prior.get_hidden_bias())
                          - torch.sum(torch.matmul(q1_pert, torch.matmul(self.prior.get_weights(), q2.t())), 1))
                          
-        return torch.mean(cross_entropy)
+        return torch.mean(cross_entropy, 0)

models/networks/hierarchicalEncoder.py
@@ -1,6 +1,5 @@
-
 """
-Autoencoders
+Hierarchical Encoder
 
 Author: Eric Drechsler (eric_drechsler@sfu.ca)
 """
@@ -11,10 +10,11 @@ import torch.nn as nn
 from models.networks.basicCoders import BasicEncoder
 from utils.dists.distributions import SpikeAndExponentialSmoother
 from utils.dists.MixtureExp import MixtureExp
+from utils.dists.MixtureExpMod import MixtureExpMod
 
 _SMOOTHER_DICT = {"SpikeExp" : SpikeAndExponentialSmoother, 
                   "MixtureExp" : MixtureExp}
-
+_SMOOTHER_MOD_DICT = {"MixtureExp" : MixtureExpMod}
 
 #logging module with handmade settings.
 from DiVAE import logging
@@ -57,7 +57,10 @@ class HierarchicalEncoder(BasicEncoder):
         self.skip_latent_layer=skip_latent_layer
 
         assert smoother in _SMOOTHER_DICT.keys(), "smoother should be one of" + str(_SMOOTHER_DICT.keys())
-        self.smoothing_distribution=_SMOOTHER_DICT[smoother]
+        self.smoothing_dist=_SMOOTHER_DICT[smoother]
+        
+        assert smoother in _SMOOTHER_MOD_DICT.keys(), "smoother should be one of" + str(_SMOOTHER_MOD_DICT.keys())
+        self.smoothing_dist_mod = _SMOOTHER_MOD_DICT[smoother]()
         
         #for each hierarchy level create a network. Input unit count will increase
         #per level.
@@ -88,8 +91,8 @@ class HierarchicalEncoder(BasicEncoder):
 
         sequential=nn.Sequential(*moduleLayers)
         return sequential
-
-    def hierarchical_posterior(self, in_data=None, is_training=True):
+    
+    def hierarchical_posterior(self, x, is_training=True):
         """ This function defines a hierarchical approximate posterior distribution. The length of the output is equal 
             to n_latent_hierarchy_lvls and each element in the list is a DistUtil object containing posterior distribution 
             for the group of latent nodes in each hierarchy level. 
@@ -106,24 +109,63 @@ class HierarchicalEncoder(BasicEncoder):
         
         posterior = []
         post_samples = []
+        
         #loop hierarchy levels. apply previously defined network to input.
         #input is concatenation of data and latent variables per layer.
         for lvl in range(self.n_latent_hierarchy_lvls):
+            
             #network of hierarchy level lvl 
             current_net=self._networks[lvl]
             #input to this level current_net
-            current_input=torch.cat([in_data]+post_samples,dim=-1)
+            current_input=torch.cat([x]+post_samples,dim=-1)
             #feed network and retrieve logit
             logits=current_net(current_input)
+            
             #build the posterior distribution for this hierarchy
             #TODO this needs a switch: training smoothing, evaluation bernoulli
-            posterior_dist = self.smoothing_distribution(logits=logits,
-                             beta=self._config.model.beta_smoothing_fct)
-            #construct the zeta values (reparameterised logits, posterior samples)
+            posterior_dist = self.smoothing_dist(logits=logits,
+                                                 beta=self._config.model.beta_smoothing_fct)
             samples=posterior_dist.reparameterise()
             posterior.append(posterior_dist)
             post_samples.append(samples)
+            
         return posterior, post_samples
+
+    def forward(self, x):
+        """ This function defines a hierarchical approximate posterior distribution. The length of the output is equal 
+            to n_latent_hierarchy_lvls and each element in the list is a DistUtil object containing posterior distribution 
+            for the group of latent nodes in each hierarchy level. 
+
+        Args:
+            input: a tensor containing input tensor.
+            is_training: A boolean indicating whether we are building a training graph or evaluation graph.
+
+        Returns:
+            posterior: a list of DistUtil objects containing posterior parameters.
+            post_samples: A list of samples from all the levels in the hierarchy, i.e. q(z_k| z_{0<i<k}, x).
+        """
+        logger.debug("ERROR Encoder::hierarchical_posterior")
+        
+        post_samples = []
+        post_logits = []
+        
+        #loop hierarchy levels. apply previously defined network to input.
+        #input is concatenation of data and latent variables per layer.
+        for lvl in range(self.n_latent_hierarchy_lvls):
+            
+            current_net=self._networks[lvl]
+            current_input=torch.cat([x]+post_samples,dim=-1)
+
+            logits=current_net(current_input)
+            post_logits.append(logits)
+            
+            beta = torch.tensor(self._config.model.beta_smoothing_fct,
+                                dtype=torch.float, requires_grad=False)
+            
+            samples=self.smoothing_dist_mod(logits, beta)
+            post_samples.append(samples)
+            
+        return beta, post_logits, post_samples
 
 if __name__=="__main__":
     logger.debug("Testing Networks")

utils/dists/MixtureExp.py
@@ -1,10 +1,10 @@
 """
-Mixture of Exponential Distributions
+Mixture of Exponential Distribution
 
 Author: Abhi (abhishek@myumanitoba.ca)
 """
 import torch
-
+from torch import nn
 from torch import log, exp
 from torch.distributions.bernoulli import Bernoulli
 
@@ -14,23 +14,7 @@ class MixtureExp(Bernoulli):
     """
     def __init__(self, logits=None, beta=None):
         super(MixtureExp, self).__init__(logits=logits)
-        self.beta = torch.tensor(beta, dtype=torch.float, requires_grad=False)
-        
-    def reparameterise(self):
-        """
-        - ICDF of mixture of exponential distributions (Eq. 3, DVAE++)
-        Returns:
-            zeta: approximate post samples
-        """
-        q = torch.sigmoid(self.logits)
-        q = torch.clamp(q, min=1e-7, max=1.-1e-7)
-        
-        rho = torch.rand(q.size())
-        b = (rho + torch.exp(-self.beta)*(q - rho))/((1. - q) - 1.)
-        c = -(q*torch.exp(-self.beta))/(1. - q)
-        
-        zeta = (-1./self.beta)*(torch.log((-b + torch.sqrt(b**2 - 4*c))/2.))
-        return zeta
+        self.beta = beta
     
     def log_pdf(self, zeta):
         """