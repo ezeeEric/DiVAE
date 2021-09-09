a98e614a6d3ed14aedb8d7fdcd668bdd2be34ba8
Eric Drechsler <eric.drechsler@cern.ch>
2021-09-09 15:10:07 -0700

    Merge slurm submission relevant changes from summary branch

---
+++ scripts/copyData.sh
@@ -0,0 +1,3 @@
+SOURCEPATH=/project/6024950/edrechsl/calogan
+TARGETPATH=/scratch/edrechsl/
+mkdir -p "$TARGETPATH" && cp -r -u "$SOURCEPATH" "$TARGETPATH"
---
+++ scripts/testRun.py
@@ -0,0 +1,72 @@
+"""
+Main executable. The run() method steers data loading, model creation, training
+and evaluation by calling the respective interfaces.
+Author: Abhishek <abhishek@myumanitoba.ca>
+Author: Eric Drechsler <eric.drechsler@cern.ch>
+"""
+
+#external libraries
+import os
+import pickle
+import datetime
+import sys
+
+import torch
+torch.manual_seed(32)
+import numpy as np
+import matplotlib.pyplot as plt
+import hydra
+from hydra.utils import instantiate
+
+from omegaconf import OmegaConf
+
+# PyTorch imports
+from torch import device, load, save
+from torch.nn import DataParallel
+from torch.cuda import is_available
+
+# Add the path to the parent directory to augment search for module
+sys.path.append(os.getcwd())
+
+# Weights and Biases
+import wandb
+
+#self defined imports
+from DiVAE import logging
+logger = logging.getLogger(__name__)
+
+from data.dataManager import DataManager
+from utils.plotting.plotProvider import PlotProvider
+from engine.engine import Engine
+from models.modelCreator import ModelCreator
+
+def runTest(config=None):
+    logger.info(os.environ)
+    if (config.device == 'gpu') and config.gpu_list:
+        logger.info('Requesting GPUs. GPU list :' + str(config.gpu_list))
+        devids = ["cuda:{0}".format(x) for x in list(config.gpu_list)]
+        logger.info("Main GPU : " + devids[0])
+
+        if is_available():
+            print(devids[0])
+            dev = device(devids[0])
+            if len(devids) > 1:
+                logger.info("Using DataParallel on {}".format(devids))
+                model = DataParallel(model, device_ids=list(config.gpu_list))
+            logger.info("CUDA available")
+        else:
+            dev = device('cpu')
+            logger.info("CUDA unavailable")
+    else:
+        logger.info('Requested CPU or unable to use GPU. Setting CPU as device.')
+        dev = device('cpu')
+
+    inFiles={
+        'gamma':    config.data.calo_input_gamma,
+        'eplus':    config.data.calo_input_eplus,        
+        'piplus':   config.data.calo_input_piplus         
+    }
+    for key, fpath in inFiles.items():
+        logger.info("{0},{1},{2}".format(key,fpath,os.path.isfile(fpath)))
+    logger.info(config.engine.n_epochs)
+    logger.info("A successful testrun")
